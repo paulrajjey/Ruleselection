5f656c3add6fde699281a9652343ca03fbafce52
rhdmAdmin <>
2019-02-11 20:25:06 -0600

    {/src/main/java/com/myspace/ruleselection/TaxonomyWorkItemHandler.java}

--- src/main/java/com/myspace/ruleselection/TaxonomyWorkItemHandler.java
+++ src/main/java/com/myspace/ruleselection/TaxonomyWorkItemHandler.java
@@ -50,7 +50,7 @@ public class TaxonomyWorkItemHandler implements WorkItemHandler ,java.io.Seriali
 	    //KieSession ksession = (KieSession) kcontext.getKieRuntime();
 	    
 	    KieServices ks = KieServices.Factory.get();
-        KieContainer kContainer = ks.getKieClasspathContainer(TaxonomyWorkItemHandler.class.getClassLoader());
+        KieContainer kContainer = ks.getKieClasspathContainer();
         StatelessKieSession stateless  = kContainer.newStatelessKieSession("rksession");	
 	    
 	   // StatelessKieSession stateless = ksession.getKieBase().newStatelessKieSession();
