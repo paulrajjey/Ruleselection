9a17e6aecf4ba77c4723ff4fea0f63f65f9a7612
rhdmAdmin <>
2019-02-12 19:05:31 -0600

    {/src/main/java/com/myspace/ruleselection/TaxonomyWorkItemHandler.java}

--- src/main/java/com/myspace/ruleselection/TaxonomyWorkItemHandler.java
+++ src/main/java/com/myspace/ruleselection/TaxonomyWorkItemHandler.java
@@ -37,7 +37,7 @@ public class TaxonomyWorkItemHandler implements WorkItemHandler ,java.io.Seriali
 
     public TaxonomyWorkItemHandler() {
 		super();
-	    kContainer = KieServices.Factory.get().newKieClasspathContainer();
+	    kContainer = KieServices.Factory.get().newKieClasspathContainer(TaxonomyWorkItemHandler.class.getClassLoader());
 		// TODO Auto-generated constructor stub
 	}
     public void executeWorkItem(WorkItem workItem, WorkItemManager manager) {
