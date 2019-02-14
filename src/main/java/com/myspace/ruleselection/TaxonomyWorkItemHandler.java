957c7a05f6b1adafd831e6de6c493996b89f168d
rhdmAdmin <>
2019-02-14 14:42:14 -0600

    {/src/main/java/com/myspace/ruleselection/TaxonomyWorkItemHandler.java}

--- src/main/java/com/myspace/ruleselection/TaxonomyWorkItemHandler.java
+++ src/main/java/com/myspace/ruleselection/TaxonomyWorkItemHandler.java
@@ -47,7 +47,10 @@ public class TaxonomyWorkItemHandler implements WorkItemHandler ,java.io.Seriali
 		java.util.List trans = (java.util.List) workItem.getParameter("facts");
 		
 		StatelessKieSession stateless  = kContainer.newStatelessKieSession("rksession");
-	 
+	    if (stateless == null){
+	        kContainer = KieServices.Factory.get().newKieClasspathContainer(TaxonomyWorkItemHandler.class.getClassLoader());
+	        stateless  = kContainer.newStatelessKieSession("rksession");
+	    }
 		stateless.setGlobal("Taxonomy", taxonomy);
 		stateless.setGlobal("TaxonomyValue", taxonomyValue);
 
